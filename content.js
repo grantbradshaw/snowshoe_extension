7d44bab7bbc15f31e022db7b8a9ac76df50cd3ab
Grant <grant.bradshaw@outlook.com>
2016-02-21 17:14:17 -0800

    App functionality only operates when toolbar present

--- content.js
+++ content.js
@@ -1,111 +1,20 @@
-(function(){
-  const invalidTargets = ['body', 'div', 'section', 'article', 'header', 'html', 'ol', 'ul'];
-
-  $(document).on('click', function(event){
-    var targeted = $(event.target);
-    if (invalidClick(targeted)) return false;
-
-    if (targeted.hasClass('selected')){
-      // Need to define a pathToSelected that accounts for specificity and generalizability
-      $(pathToSelected).removeClass('selected');
-      removeDetails(pathToSelected);
-      console.log(scrapeResults);
-    } else {
-      var generalize = prompt("Should we select all elements like this one?");
-      
-      if (generalize === 'yes'){
-        var pathToSelected = selectSame(targeted);
-      } else {
-        var pathToSelected = targeted.getPath();
-      }
-
-      $(pathToSelected).addClass('selected');
-      scrapeDetails(pathToSelected);
-      console.log(scrapeResults);
-    }
-  });
-
-  function invalidClick(target){
-    var targetTag = target.prop("tagName").toLowerCase();
-    return $.inArray(targetTag, invalidTargets) >= 0 || target.hasClass('snowshoe');
-  }
-
-  function scrapeDetails(path){
-    var details = {
-      'path': path,
-      'elemContents': []
-    }
-    $(path).each(function(i, ele){
-      details.elemContents.push($(ele).text());
-    });
-    scrapeResults.scrapes.push(details);
-  }
-
-  function removeDetails(path){
-    scrapeResults.scrapes.forEach(function(scrape, i){
-      if (scrape.path === path) {
-        scrapeResults.scrapes.splice(i, 1);
-      }
-    });
-  }
-
-  function selectSame(elem){
-    var parentsEles = [];
-    elemPath = filterEles(elem.parents());
-    $(elemPath).addBack().each(function() {
-      var entry = this.tagName.toLowerCase();
-      if (this.className) {
-        entry += "." + this.className.replace(/ /g, '.');
-      }
-      parentsEles.push(entry);
-    });
-    return parentsEles.join(" ") + ' ' + elem.prop("tagName").toLowerCase();
-  }
-
-  function filterEles(elemArray){
-    return elemArray.not('body').not('html');
-  } 
-
-  jQuery.fn.getPath = function () {
-    if (this.length != 1) throw 'Requires one element.';
-
-    var path, node = this;
-    while (node.length) {
-        var realNode = node[0], name = realNode.localName;
-        if (!name) break;
-        name = name.toLowerCase();
-
-        var parent = node.parent();
-
-        var siblings = parent.children(name);
-        if (siblings.length > 1) { 
-            name += ':eq(' + siblings.index(realNode) + ')';
-        }
-
-        path = name + (path ? '>' + path : '');
-        node = parent;
-    }
-    return path;
-  };
-
-})();
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if ( request.message === "clicked_browser_action" ) {
       if ($('.toolbar').length > 0){
           $('.toolbar').remove();
+          $('.selected').removeClass('selected');
+          $(document).off('click', select_handler);
       } else {
         var frame = $('<div>').addClass('toolbar');
         $('body').prepend(frame);
+        $(document).on('click', select_handler);
       }
       var button_export = $('<button type="button" class="export snowshoe">Export</button>');
       frame.append(button_export)
     }
   }
 );
-
-
 
 var scrapeResults = {
   'url': window.location.href,
@@ -130,9 +39,100 @@ $(document).on('click', '.export', function(){
       $('.toolbar').remove();
       scrapeResults.track_name = '';
       scrapeResults.scrapes = [];
+      $('.selected').removeClass('selected');
+      $(document).off('click', select_handler);
     }
   }
 });
+
+const invalidTargets = ['body', 'div', 'section', 'article', 'header', 'html', 'ol', 'ul'];
+
+function select_handler(event){
+  var targeted = $(event.target);
+  if (invalidClick(targeted)) return false;
+
+  if (targeted.hasClass('selected')){
+    // Need to define a pathToSelected that accounts for specificity and generalizability
+    $(pathToSelected).removeClass('selected');
+    removeDetails(pathToSelected);
+    console.log(scrapeResults);
+  } else {
+    var generalize = prompt("Should we select all elements like this one?");
+    
+    if (generalize === 'yes'){
+      var pathToSelected = selectSame(targeted);
+    } else {
+      var pathToSelected = targeted.getPath();
+    }
+
+    $(pathToSelected).addClass('selected');
+    scrapeDetails(pathToSelected);
+    console.log(scrapeResults);
+  }
+}
+
+function invalidClick(target){
+  var targetTag = target.prop("tagName").toLowerCase();
+  return $.inArray(targetTag, invalidTargets) >= 0 || target.hasClass('snowshoe');
+}
+
+function scrapeDetails(path){
+  var details = {
+    'path': path,
+    'elemContents': []
+  }
+  $(path).each(function(i, ele){
+    details.elemContents.push($(ele).text());
+  });
+  scrapeResults.scrapes.push(details);
+}
+
+function removeDetails(path){
+  scrapeResults.scrapes.forEach(function(scrape, i){
+    if (scrape.path === path) {
+      scrapeResults.scrapes.splice(i, 1);
+    }
+  });
+}
+
+function selectSame(elem){
+  var parentsEles = [];
+  elemPath = filterEles(elem.parents());
+  $(elemPath).addBack().each(function() {
+    var entry = this.tagName.toLowerCase();
+    if (this.className) {
+      entry += "." + this.className.replace(/ /g, '.');
+    }
+    parentsEles.push(entry);
+  });
+  return parentsEles.join(" ") + ' ' + elem.prop("tagName").toLowerCase();
+}
+
+function filterEles(elemArray){
+  return elemArray.not('body').not('html');
+} 
+
+jQuery.fn.getPath = function () {
+  if (this.length != 1) throw 'Requires one element.';
+
+  var path, node = this;
+  while (node.length) {
+      var realNode = node[0], name = realNode.localName;
+      if (!name) break;
+      name = name.toLowerCase();
+
+      var parent = node.parent();
+
+      var siblings = parent.children(name);
+      if (siblings.length > 1) { 
+          name += ':eq(' + siblings.index(realNode) + ')';
+      }
+
+      path = name + (path ? '>' + path : '');
+      node = parent;
+  }
+  return path;
+};
 
 function createCORSRequest(method, url){
   var xhr = new XMLHttpRequest();
